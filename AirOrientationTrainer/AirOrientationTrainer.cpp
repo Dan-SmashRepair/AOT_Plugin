ce21b39b7f2c566c256c22477d9176e818f080ba
Dan (Omus) <46916734+Dan-SmashRepair@users.noreply.github.com>
2024-01-14 15:25:44 +1100

    Add files via upload

--- AirOrientationTrainer/AirOrientationTrainer.cpp
+++ AirOrientationTrainer/AirOrientationTrainer.cpp
@@ -36,7 +36,7 @@ void AirOrientationTrainer::onLoad()
 		.addOnValueChanged([this](std::string oldValue, CVarWrapper cvar) {
 		trainingMode = cvar.getIntValue();
 	});
-	cvarManager->registerCvar("AOT_difficulty", "10", "Difficulty", true, true, 5, true, 25);
+	cvarManager->registerCvar("AOT_difficulty", "14", "Difficulty", true, true, 5, true, 25);
 
 	cvarManager->registerCvar("AOT_best_times", "1080,1080,1080,1080,1080,1080,1080,1080,1080,1080", "High scores", false);
 	
@@ -102,7 +102,7 @@ void AirOrientationTrainer::reset() {
 			initCarYaw = dist(rd);
 			initCarRoll = dist(rd);
 			targetPitch = dist2(rd);
-			targetYaw = -abs(dist(rd));
+			targetYaw = -abs(dist(rd)); // only yaw pointing towards blue goal, less confusing and still allows all possible rotations
 			targetRoll = dist(rd);
 			firstNewTrain = false;
 		}
@@ -146,37 +146,7 @@ void AirOrientationTrainer::Render(CanvasWrapper canvas)
 
 	elapsedTime++; // increment round timer
 	
-	// difficulty 10 means you have to be within 10% of pitch, yaw and roll. Lower = harder
-	CVarWrapper difficultyCVar = cvarManager->getCvar("AOT_difficulty");
-	if (!difficultyCVar) { return; }
-	int difficulty = difficultyCVar.getIntValue();
-
-	// go next if target is achieved
-	int rot_accuracy = difficulty * 654;
-	(tickFlipFlop == true) ? tickFlipFlop = false : tickFlipFlop = true; // only reset every 2nd tick to avoid edge cases
-	if (abs(targetPitch - carRotation.Pitch) < rot_accuracy / 2 && tickFlipFlop) {
-		if (abs(targetYaw - carRotation.Yaw) < rot_accuracy || abs(targetYaw) >= 32768 - rot_accuracy && abs(carRotation.Yaw) >= 32768 - rot_accuracy) {
-			if (abs(targetRoll - carRotation.Roll) < rot_accuracy || abs(targetRoll) >= 32768 - rot_accuracy && abs(carRotation.Roll) >= 32768 - rot_accuracy) {
-				if (trainingMode == 0) { // if new best time in default training, update score and save to file
-					newPB = false;
-					if (bestTimes.at(trainingCurrent) > elapsedTime) {
-						bestTimes.at(trainingCurrent) = elapsedTime;
-						newPB = true;
-						std::ostringstream oss;
-						std::copy(std::begin(bestTimes), std::end(bestTimes), std::ostream_iterator<int>(oss, ","));
-						std::string str(oss.str());
-						CVarWrapper bestTimesCVar = cvarManager->getCvar("AOT_best_times");
-						if (!bestTimesCVar) { return; }
-						bestTimesCVar.setValue(str);
-						cvarManager->executeCommand("writeconfig", false); // false just means don't log the writeconfig
-					}
-				}
-				reset();
-			}
-		}
-	}
-
-	// calculating then visualizing target orientation
+	// calculate target 3D points to render
 	Quat targetQuat = RotatorToQuat(Rotator{ targetPitch,  targetYaw, targetRoll });
 	Vector frontCarVec = RotateVectorWithQuat(Vector{ 80, 0, 0 }, targetQuat) + Vector{ 0, 2000, 1000 };
 	Vector leftCarVec = RotateVectorWithQuat(Vector{ -20, 20, 0 }, targetQuat) + Vector{ 0, 2000, 1000 };
@@ -184,7 +154,45 @@ void AirOrientationTrainer::Render(CanvasWrapper canvas)
 	Vector targetRollVector = RotateVectorWithQuat(Vector{ 0, 0, -30 }, targetQuat) + frontCarVec;
 	Vector rollArrow1 = RotateVectorWithQuat(Vector{ 0, 5, 0 }, targetQuat) + frontCarVec;
 	Vector rollArrow2 = RotateVectorWithQuat(Vector{ 0, -5, 0 }, targetQuat) + frontCarVec;
+	
+	// calculate actual car vectors and angle to target
+	Quat carQuat = RotatorToQuat(car.GetRotation());
+	Vector actualForwardDirection = RotateVectorWithQuat(Vector{ 1, 0, 0 }, carQuat);
+	Vector actualBelowDirection = RotateVectorWithQuat(Vector{ 0, 0, -1 }, carQuat);
+	Vector targetForwardDirection = RotateVectorWithQuat(Vector{ 1, 0, 0 }, targetQuat);
+	Vector targetBelowDirection = RotateVectorWithQuat(Vector{ 0, 0, -1 }, targetQuat);
+	float forwardAngle = std::acos(actualForwardDirection.X * targetForwardDirection.X + actualForwardDirection.Y * targetForwardDirection.Y + actualForwardDirection.Z * targetForwardDirection.Z);
+	float belowAngle = std::acos(actualBelowDirection.X * targetBelowDirection.X + actualBelowDirection.Y * targetBelowDirection.Y + actualBelowDirection.Z * targetBelowDirection.Z);
 
+	// difficulty 10 means angle to target needs to be within 10%. Lower = harder
+	CVarWrapper difficultyCVar = cvarManager->getCvar("AOT_difficulty");
+	if (!difficultyCVar) { return; }
+	int difficulty = difficultyCVar.getIntValue();
+
+	// go next if target is achieved
+	float rot_accuracy = difficulty * std::_Pi_val / 100;
+	(tickFlipFlop == true) ? tickFlipFlop = false : tickFlipFlop = true; // only reset every 2nd tick to avoid edge cases
+	if (forwardAngle < rot_accuracy && tickFlipFlop) {
+		if (belowAngle < rot_accuracy) {
+			if (trainingMode == 0) { // if new best time in default training, update score and save to file
+				newPB = false;
+				if (bestTimes.at(trainingCurrent) > elapsedTime) {
+					bestTimes.at(trainingCurrent) = elapsedTime;
+					newPB = true;
+					std::ostringstream oss;
+					std::copy(std::begin(bestTimes), std::end(bestTimes), std::ostream_iterator<int>(oss, ","));
+					std::string str(oss.str());
+					CVarWrapper bestTimesCVar = cvarManager->getCvar("AOT_best_times");
+					if (!bestTimesCVar) { return; }
+					bestTimesCVar.setValue(str);
+					cvarManager->executeCommand("writeconfig", false); // false just means don't log the writeconfig
+				}
+			}
+			reset();
+		}
+	}
+
+	// calculate 2D projection points from 3D points
 	Vector2 centerProj = canvas.Project(Vector{ 0, 2000, 1000 });
 	Vector2 startProj = canvas.Project(frontCarVec);
 	Vector2 leftCarProj = canvas.Project(leftCarVec);
@@ -193,28 +201,43 @@ void AirOrientationTrainer::Render(CanvasWrapper canvas)
 	Vector2 rollArr1Proj = canvas.Project(rollArrow1);
 	Vector2 rollArr2Proj = canvas.Project(rollArrow2);
 	
+	// render target lines
 	canvas.SetColor(255, 255, 0, 255);
 	canvas.DrawLine(startProj, centerProj);
 	canvas.DrawLine(startProj, leftCarProj);
 	canvas.DrawLine(startProj, rightCarProj);
-	canvas.SetColor(255, 64, 0, 255);
+	int green;
+	(targetRollVector.Y - frontCarVec.Y < 0) ? green = 64 : green = 128; // different orange if arrow pointing backwards, helps to understand arrow directions
+	canvas.SetColor(255, green, 0, 255);
 	canvas.DrawLine(targetRollProj, startProj);
 	canvas.DrawLine(targetRollProj, rollArr1Proj);
 	canvas.DrawLine(targetRollProj, rollArr2Proj);
 	
-	// tutorial text. Only shows until one of the first few levels is completed in under 5s
+	// tutorial text. Only show until one of the first few levels is completed in under 5s
 	if (bestTimes.at(0) > 600 && bestTimes.at(1) > 600 && bestTimes.at(2) > 600){
 		canvas.SetColor(255, 255, 0, 255);
 		canvas.SetPosition(Vector2F{ 150.0, 220.0 });
-		canvas.DrawString("Align the front of your car with the yellow arrow, wheels toward orange.");
+		canvas.DrawString("Align the front of your car with the yellow arrow, wheels toward orange.", 2.0, 2.0, false);
 	}
+	
+	// uncomment to show car angle to target % numbers on screen
+	/*std::string fa = std::to_string(forwardAngle / std::_Pi_val * 100);
+	std::string ba = std::to_string(belowAngle / std::_Pi_val * 100);
+	fa.erase(fa.length() - 7);
+	ba.erase(ba.length() - 7);
+	canvas.SetColor(255, 255, 0, 255);
+	canvas.SetPosition(Vector2F{ 1.0, 590.0 });
+	canvas.DrawString("Front car angle: " + fa + "%", 1.0, 1.0, false);
+	canvas.SetPosition(Vector2F{ 1.0, 610.0 });
+	canvas.DrawString("Wheel angle: " + ba + "%", 1.0, 1.0, false);*/
+
 	// uncomment to show car rotation numbers on screen
 	/*canvas.SetColor(255, 255, 0, 255);
-	canvas.SetPosition(Vector2F{ 1.0, 590.0 });
+	canvas.SetPosition(Vector2F{ 1.0, 660.0 });
 	canvas.DrawString("Pitch: " + std::to_string(carRotation.Pitch), 1.0, 1.0, false);
-	canvas.SetPosition(Vector2F{ 1.0, 610.0 });
+	canvas.SetPosition(Vector2F{ 1.0, 680.0 });
 	canvas.DrawString("Yaw  : " + std::to_string(carRotation.Yaw), 1.0, 1.0, false);
-	canvas.SetPosition(Vector2F{ 1.0, 630.0 });
+	canvas.SetPosition(Vector2F{ 1.0, 700.0 });
 	canvas.DrawString("Roll: " + std::to_string(carRotation.Roll), 1.0, 1.0, false);*/
 
 	if (elapsedTime > 240) { newPB = false; } // display "NEW PB!" onscreen for a maximum of 2s
@@ -227,7 +250,7 @@ void AirOrientationTrainer::Render(CanvasWrapper canvas)
 		canvas.SetPosition(Vector2F{ 500.0, 50.0 });
 		canvas.DrawString("Replaying round, your time was " + y + "s", 3.0, 3.0, false);
 	}
-	// building the scoreboard
+	// build scoreboard
 	if (trainingMode == 0) {
 		canvas.SetColor(255, 80, 0, 255);
 		canvas.SetPosition(Vector2F{ 1.0, 260.0 });
